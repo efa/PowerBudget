ee3e68546b68c74a35f7c78f130bac6339dd38e5
Valerio Messina <efa@iol.it>
2025-08-18 12:22:03 +0200

    heavy modification to makePkg.sh to be general minor modification to Makefiles

--- src/makePkg.sh
+++ src/makePkg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# PowerBudget v0.00.01a 2025/08/16 calculate power dissipation and budget
+# PowerBudget v0.00.01a 2025/08/18 calculate power dissipation and budget
 # makePkg.sh: Copyright 2005-2025 Valerio Messina efa@iol.it
 # makePkg is part of PowerBudget
 # PowerBudget is free software: you can redistribute it and/or modify
@@ -18,33 +18,40 @@
 # Script to generate a Linux|Mingw|MXE|OSX package of 'PowerBudget'
 # used on: Linux=>bin64, Linux=>bin32, Linux=>macOS64
 #          MinGw64=>bin64, MinGw32=>bin32, MXE64=>bin64, MXE32=>bin32
+# 7z Win   packages: $AppName/ (bin+assets) src/
+# tar.gz   packages: $AppName/ (bin+assets) src/
+# AppImage packages: AppDir/   usr/bin
+# macOS    packages: DiskImage/$AppName.app/Contents/MacOS/
 #
-# Syntax: $ makePkg.sh [-y] Linux|WinMxe|WinMgw|MacOS [32|64]
+# Syntax: $ makePkg.sh [-y] Linux|WinMxe|WinMgw|Osx [32|64]
 
-makever=2025-08-16
+makever=2025-08-18
+
+APP="PowerBudget"    # app name
+BINRAD="powerBudget" # binary radix
+BINPATH=`pwd`/.. # where find the just build binary
+SRCPATH="." # source package path
+RESPATH=".." # other resources (readme, icons, desktop files) to copy in packages
+TMPPATH="../pkg" # temp path where create uncompressed dir,AppDir,DiskImage
+DSTPATH="../.." # path where write the final Linux|Mingw/MXE|OSX package
 
 DEPSPATHMGW64="/mingw64/bin" # path of DLLs needed to generate the Mingw64 package
 DEPSPATHMGW32="/mingw32/bin" # path of DLLs needed to generate the Mingw32 package
-DEPSLISTMGW="" # list of dll for MinGW
+DEPSLISTMGW="" # list of dll for MinGW cross-build dynamic link
 
-APP="PowerBudget"    # app name
-SRCPKG=".." # source package path
-BIN="powerBudgetGui" # binary
-DSTPATH=".." # path where create the Linux|Mingw/MXE|OSX package directory
-
-echo "makePkg.sh: create a Linux|WinMxe|WinMgw|MacOS package for $APP ..."
+echo "makePkg.sh: create a Linux|WinMxe|WinMgw|Osx package for $APP ..."
 
 # check for external dependency compliance
 flag=0
-for extCmd in 7z chmod cp cut date genisoimage grep mkdir mv pwd rm tar uname wget ; do
+for extCmd in 7z chmod cp cut date getconf grep mkdir mv pwd realpath rm tar uname wget ; do
    exist=`which $extCmd 2> /dev/null`
    if (test "" = "$exist") then
-      echo "Required external dependency: "\"$extCmd\"" unsatisfied!"
+      echo "ERROR: makePkg.sh Required external dependency: "\"$extCmd\"" unsatisfied!"
       flag=1
    fi
 done
 if [[ "$flag" = 1 ]]; then
-   echo "ERROR: Install the required packages and retry. Exit"
+   echo "ERROR: makePkg.sh Install the required packages and retry. Exit"
    exit
 fi
 
@@ -52,20 +59,20 @@ if (test "$1" = "-y") then
    batch=1
    shift
 fi
-if [[ "$1" = "" || "$1" != "Linux" && "$1" != "WinMxe" && "$1" != "WinMgw" && "$1" != "MacOS" ]]; then
+if [[ "$1" = "" || "$1" != "Linux" && "$1" != "WinMxe" && "$1" != "WinMgw" && "$1" != "Osx" ]]; then
    echo "ERROR: makePkg.sh unsupported/miss target platform to create package"
-   echo "Syntax: $ makePkg.sh [-y] Linux|WinMxe|WinMgw|MacOS [32|64]"
+   echo "Syntax: $ makePkg.sh [-y] Linux|WinMxe|WinMgw|Osx [32|64]"
    echo "          -y for batch execution without confirmations"
    exit
 fi
 
 #exist=`which gtk-mac-bundler 2> /dev/null`
-#if (test "$1" = "MacOS" && test "" = "$exist") then
+#if (test "$1" = "Osx" && test "" = "$exist") then
 #   echo "ERROR: makePkg.sh depend on 'gtk-mac-bundler' to generate for macOS. Exit"
 #   exit
 #fi
 exist=`which osxcross-dmg 2> /dev/null`
-if (test "$1" = "OSX64" && test "" = "$exist") then
+if (test "$1" = "Osx" && test "" = "$exist") then
    echo "ERROR: makePkg.sh depend on 'osxcross-dmg' to generate for macOS. Exit"
    exit
 fi
@@ -78,52 +85,48 @@ else
    BIT="$2"
 fi
 
-BIN="$BIN$PKG$BIT"
-if [[ -f $BIN ]]; then
-   mkdir -p $DSTPATH
-else
-   echo "ERROR: makePkg.sh cannot find just built binary './$BIN'. Exit"
+BINCLI="${BINRAD}$PKG$BIT"
+BINGUI="${BINRAD}Gui$PKG$BIT"
+BINPATH=`realpath "$BINPATH"`
+SRCPATH=`realpath "$SRCPATH"`
+RESPATH=`realpath "$RESPATH"`
+TMPPATH=`realpath "$TMPPATH"`
+DSTPATH=`realpath "$DSTPATH"`
+if [[ ! -d "$BINPATH" ]]; then
+   echo "ERROR: makePkg.sh cannot find binary path '$BINPATH'. Exit"
    exit
 fi
-
-if (test "$CPU" = "x86_64" && test "$BIT" = "32") then
-   CPU=i686
+if [[ ! -d "$SRCPATH" ]]; then
+   echo "ERROR: makePkg.sh cannot find source path '$SRCPATH'. Exit"
+   exit
 fi
-if (test "$PKG" = "WinMxe" || test "$PKG" = "WinMgw") then
-   EXT=".exe"
+if [[ ! -d "$RESPATH" ]]; then
+   echo "ERROR: makePkg.sh cannot find resource path '$RESPATH'. Exit"
+   exit
+fi
+if (test "$BINCLI" = "" && test "$BINGUI" = "") then
+   echo "ERROR: makePkg.sh cannot be both NULL 'BINCLI' and 'BINGUI'. Exit"
+   exit
+fi
+if (test "$BINCLI" != "") then
+   if [[ ! -f "$BINPATH/$BINCLI" ]]; then
+      echo "ERROR: makePkg.sh cannot find just built binary '$BINPATH/$BINCLI'. Exit"
+      exit
+   fi
+fi
+if (test "$BINGUI" != "") then
+   if [[ ! -f "$BINPATH/$BINGUI" ]]; then
+      echo "ERROR: makePkg.sh cannot find just built binary '$BINPATH/$BINGUI'. Exit"
+      exit
+   fi
 fi
 
 OS=`uname`
 if (test "$OS" != "Darwin") then
    OS=`uname -o`  # Msys or GNU/Linux, illegal on macOS
 fi
-VER=`grep SourceVersion powerbLib.h | cut -d' ' -f3 | tr -d '."'`
-DATE=`date -I`
-BINPATH=`pwd`
-TGT=$PKG
-if [[ "$OS" = "Msys" ]]; then
-   if [[ "$BIT" = "64" ]]; then
-      DEPSRC=$DEPSPATHMGW64
-   fi
-   if [[ "$BIT" = "32" ]]; then
-      DEPSRC=$DEPSPATHMGW32
-   fi
-fi
-UPKGPAT="" # Uncompressed Package Path
-if [[ "$PKG" = "MacOS" ]]; then
-   TGT=Osx
-   UPKGPAT="DiskImage/$APP.app/Contents/MacOS"
-   DSTSP="$UPKGPAT"
-fi
-if [[ "$PKG" = "Linux" ]]; then
-   TGT=Linux
-   UPKGPAT="usr/bin"
-   DSTSP="usr/src"
-fi
-DSTPKG="${APP}_${VER}_${DATE}_${TGT}_${CPU}_${BIT}bit"
-
 if [[ "$OS" != "Msys" && "$OS" != "GNU/Linux" ]]; then
-   echo "ERROR makePkg.sh: work in Linux|WinMxe(Linux)|WinMgw only"
+   echo "ERROR makePkg.sh: work in Linux|MXE(Linux)|WinMgw only"
    exit
 fi
 if [[ "$OS" = "Msys" && "$PKG" != "WinMgw" ]]; then
@@ -131,115 +134,270 @@ if [[ "$OS" = "Msys" && "$PKG" != "WinMgw" ]]; then
    exit
 fi
 
-if [[ "$OS" = "GNU/Linux" && "$PKG" != "Linux" && "$PKG" != "WinMxe" && "$PKG" != "MacOS" ]]; then
+if [[ "$OS" = "GNU/Linux" && "$PKG" != "Linux" && "$PKG" != "WinMxe" && "$PKG" != "Osx" ]]; then
    echo "ERROR makePkg.sh: Unsupported target package:$PKG on Linux"
    exit
 fi
 
-echo "DATE   : $DATE"    # today
-echo "APP    : $APP"     # app name
-echo "VER    : $VER"     # src ver
-echo "PKG    : $PKG"     # input par
-echo "TGT    : $TGT"     # req target OS
-echo "TGTBIT : $BIT"     # req target BIT
-echo "TGTCPU : $CPU"     # target CPU
-echo "HOSTOS : $OS"      # current OS
-echo "SRCPKG : $SRCPKG"  # packaging file source path
-echo "BINPATH: $BINPATH" # binary path
-echo "BIN    : $BIN"     # binary file
-if (test "$DEPSRC" != "") then
-echo "SRCDEP : $DEPSRC"  # dependancy path (MinGw)
+DATE=`date -I`
+VER=`grep SourceVersion powerbLib.h | cut -d' ' -f3 | tr -d '."'` # M.mm.bb ==> Mmmbb
+TGT=$PKG
+if [[ "$OS" = "Msys" ]]; then
+   if [[ "$BIT" = "64" ]]; then
+      DEPPATH=$DEPSPATHMGW64
+   fi
+   if [[ "$BIT" = "32" ]]; then
+      DEPPATH=$DEPSPATHMGW32
+   fi
 fi
+TMPBINP="" # temp bin Package Path
+if [[ "$PKG" = "Osx" ]]; then
+   TGT=Osx
+   TMPBINP="DiskImage/$APP.app/Contents/MacOS"
+fi
+if [[ "$PKG" = "Linux" ]]; then
+   TGT=Linux
+   TMPBINP="usr/bin"
+fi
+if (test "$CPU" = "x86_64" && test "$BIT" = "32") then
+   CPU=i686
+fi
+if (test "$PKG" = "WinMxe" || test "$PKG" = "WinMgw") then
+   EXT=".exe"
+fi
+PKGNAME="${APP}_${VER}_${DATE}_${TGT}_${CPU}_${BIT}bit"
+echo "DATE   : $DATE"    # today
+echo "HOSTOS : $OS"      # current OS
+echo "APP    : $APP"     # app name
+echo "VER    : $VER"     # app src ver
+echo "PKG    : $PKG"     # input parameter
+echo "TGT    : $TGT"     # target OS
+echo "TGTCPU : $CPU"     # target CPU
+echo "TGTBIT : $BIT"     # target BIT
+echo "BINRAD : $BINRAD"  # binary radix
+if (test "$BINCLI" != "") then
+echo "BINCLI : $BINCLI"  # binary file for CLI
+fi
+if (test "$BINGUI" != "") then
+echo "BINGUI : $BINGUI"  # binary file for GUI
+fi
+if (test "$EXT" != "") then
+echo "EXT    : $EXT"     # binary extension (Mxe|MinGw)
+fi
+echo "BINPATH: $BINPATH" # binary path
+echo "SRCPATH: $SRCPATH" # sources path
+echo "RESPATH: $RESPATH" # resources path
+if (test "$DEPPATH" != "") then
+echo "DEPPATH: $DEPPATH" # dependancy path (MinGw)
+fi
+echo "TMPPATH: $TMPPATH" # temp path where create uncompressed dir,AppDir,DiskImage
+echo "TMPBINP: $TMPBINP" # tmp bin path. eg. usr/bin | DiskImage/$APP.app/Contents/MacOS
 echo "DSTPATH: $DSTPATH" # package creation path
-echo "DSTSP  : $DSTSP"   # app sources destination path
-echo "UPKGPAT: $UPKGPAT" # package uncompressed creation path
-echo "DSTPKG : $DSTPKG"  # package name
+echo "PKGNAME: $PKGNAME" # package name
 if (test "$batch" != "1") then
    read -p "Proceed? A key to continue"
 fi
 echo ""
 
 echo "makePkg: Creating $APP $VER package for $TGT$BIT.$CPU ..."
-cp -a ../Readme.txt ../README.md
+cp -a $RESPATH/README.txt $RESPATH/README.md
 
-if (test "$PKG" = "MacOS") then
+echo "Creating temp and destination path if miss ..."
+if [[ ! -d "$TMPPATH" ]]; then
+   mkdir -p "$TMPPATH"
+fi
+if [[ ! -d "$DSTPATH" ]]; then
+   mkdir -p "$DSTPATH"
+fi
+
+echo "Removing old temp files if present ..."
+if (test "$TGT" = "Linux" || test "$TGT" = "WinMxe" || test "$TGT" = "WinMgw") then
+   if [[ -d "$TMPPATH/$APP" ]]; then
+      echo "Removing old $APP ..."
+      rm -rf "$TMPPATH/$APP"
+   fi
+fi
+if (test "$TGT" = "Linux") then
+   if [[ -d "$TMPPATH/AppDir" ]]; then
+      echo "Removing old AppDir ..."
+      rm -rf "$TMPPATH/AppDir"
+   fi
+fi
+
+echo "Removing old temp packages if present ..."
+if (test "$TGT" = "Linux") then
+   if [[ -f "$TMPPATH/$PKGNAME.tgz" ]]; then
+      echo "Removing old tgz Package ..."
+      rm -rf "$TMPPATH/$PKGNAME.tgz"
+   fi
+   if [[ -f "$TMPPATH/$PKGNAME.AppImage" ]]; then
+      echo "Removing old AppImage ..."
+      rm -f "$TMPPATH/$PKGNAME.AppImage"
+   fi
+fi
+if (test "$TGT" = "WinMxe" || test "$TGT" = "WinMgw") then
+   if [[ -f "$TMPPATH/$PKGNAME.7z" ]]; then
+      echo "Removing old 7z Package ..."
+      rm -rf "$TMPPATH/$PKGNAME.7z"
+   fi
+fi
+if (test "$TGT" = "Osx") then
+   if [[ -f "$TMPPATH/$APP.dmg" ]]; then
+      echo "Removing old dmg Package ..."
+      rm -rf "$TMPPATH/$APP.dmg"
+   fi
+fi
+
+if (test "$TGT" = "Osx") then
    if (test "$BIT" = "32") then
-      echo "Unsupported 32 bit on MacOS"
+      echo "Unsupported 32 bit on Osx"
       exit
    fi
+   if (test "$BINCLI" != "") then
+      cp -a $BINPATH/$BINCLI $TMPPATH # bin
+   fi
+   if (test "$BINGUI" != "") then
+      cp -a $BINPATH/$BINGUI $TMPPATH # bin
+   fi
+   cp -a $RESPATH/$APP.icns $TMPPATH # need to be near bin
+   cd $TMPPATH
    #gtk-mac-bundler $APP.bundle
-   cp -a ${APP}MacOS $APP.icns AppDir
-   cd AppDir
-   osxcross-dmg -rw ${APP}MacOS $APP $VER
-   rm uncompressed.dmg ${APP}MacOS $APP.icns
-   mv $APP$VER.dmg ..
-   cd ..
-   #mkdir $APP.app
-   #cd ../..
-   #AppName=powerBudgetGuiMacOS64
-   #ls -l $AppName.app
-   #tar -cf $AppName.app.tgz $AppName.app
-   #wp=0 # write protect
-   #echo "Generating uncompressesd DMG ..."
-   #rm -rf DiskImage 2>/dev/null
-   #mkdir DiskImage
-   #mv $AppName.app DiskImage
-   #rm $AppName$VER.dmg 2> /dev/null
-   #if (test "$wp" = 1) then
-   #   genisoimage -V $AppName -D -r -apple -no-pad -o $AppName$VER.dmg DiskImage
-   #else
-   #   genisoimage -V $AppName -D -R -apple -no-pad -o $AppName$VER.dmg DiskImage
-   #fi
-   #rm -rf DiskImage
-   #echo "Compressing DMG ..."
-   #mv $AppName$VER.dmg uncompressed.dmg
-   # dmg from: https://github.com/fanquake/libdmg-hfsplus
-   #dmg uncompressed.dmg $AppName$VER.dmg
-   #rm uncompressed.dmg
-   #echo "$AppName$VER.dmg created."
+   osxcross-dmg -rw $BIN $APP $VER # create compressed MacOS package
+   rm uncompressed.dmg $APP.icns # remove copied
+   rm $BINCLI $BINGUI 2>/dev/null # remove copied
+   mv $APP$VER.dmg $DSTPATH
    exit
 fi
 
-if [[ -d "$DSTPATH/AppDir" ]]; then
-   echo "Removing old AppDir ..."
-   rm -rf "$DSTPATH/AppDir"
-fi
-mkdir -p "$DSTPATH/AppDir/$UPKGPAT"
-
-if [[ -f "$DSTPATH/AppImage/$DSTPKG.AppImage" ]]; then
-   echo "Removing old AppImage ..."
-   rm -f "$DSTPATH/AppImage/$DSTPKG.AppImage"
-fi
-if [[ -d "$DSTPATH/AppImage/AppDir" ]]; then
-   echo "Removing old AppImageDir ..."
-   rm -rf "$DSTPATH/AppImage/AppDir"
+echo "Creating package directory structure ..."
+mkdir -p $TMPPATH/$APP/src
+if (test "$TGT" = "Linux") then
+   mkdir -p "$TMPPATH/AppDir/$TMPBINP"
 fi
 
-if [[ -d $DSTPATH/$DSTPKG ]]; then
-   echo "Removing old DSTPKG ..."
-   rm -rf "$DSTPATH/$DSTPKG"
+echo "Copying sources ..."
+cp -a $SRCPATH/*.h        $TMPPATH/$APP/src/
+cp -a $SRCPATH/*.c        $TMPPATH/$APP/src/
+cp -a $SRCPATH/Makefile*  $TMPPATH/$APP/src/
+cp -a $SRCPATH/makePkg.sh $TMPPATH/$APP/src/
+cp -a $SRCPATH/*.ini      $TMPPATH/$APP/src/   # if needed
+#cp -aL $SRCPATH/$APP.png   $TMPPATH/$APP/src/   # if needed
+
+echo "Copying assets ..."
+cp -a $RESPATH/powerb.ini   $TMPPATH/$APP/   # if needed
+if (test "$TGT" = "Linux") then
+cp -a $RESPATH/powerb.ini   $TMPPATH/AppDir/$TMPBINP   # if needed
+cp -a $RESPATH/$APP.desktop $TMPPATH/$APP/
+cp -a $RESPATH/$APP.desktop $TMPPATH/AppDir/
+cp -a $RESPATH/$APP.png     $TMPPATH/$APP/
+cp -a $RESPATH/$APP.png     $TMPPATH/AppDir/
 fi
-mkdir -p $APP/src
+if (test "$TGT" = "WinMxe" || test "$TGT" = "WinMgw") then
+cp -a $RESPATH/$APP.ico     $TMPPATH/$APP/
+fi
 
-cd ..
-cp -a Readme.txt LICENSE src/AppDir/usr/bin
-cp -a Readme.txt LICENSE src/PowerBudget
-cp -a powerBudget${PKG}${BIT}${EXT}    src/AppDir/usr/bin/
-cp -a powerBudget${PKG}${BIT}${EXT}    src/PowerBudget/
-cp -a powerBudgetGui${PKG}${BIT}${EXT} src/AppDir/usr/bin/
-cp -a powerBudgetGui${PKG}${BIT}${EXT} src/PowerBudget/
-cd src
-cp -a powerb.ini AppDir/usr/bin/src
-cp -a powerb.ini PowerBudget/src
-cp -a makePkg.sh PowerBudget/src
-cp -a *.h *.c    PowerBudget/src
-cp -a Makefile*  PowerBudget/src
-cp -a PowerBudget.png powerBudget.desktop AppDir
-cp -a PowerBudget.png powerBudget.desktop PowerBudget/src
-cp -a PowerBudget.ico PowerBudget.icns Info.plist PowerBudget/src 2>/dev/null
+echo "Copying docs ..."
+cp -a $RESPATH/COPYING       $TMPPATH/$APP/
+cp -a $RESPATH/README.txt    $TMPPATH/$APP/   # if needed
+#cp -a $RESPATH/Changelog.txt $TMPPATH/$APP/   # if needed
+cp -a $RESPATH/${APP}GUI.png $TMPPATH/$APP/   # if needed
+if (test "$TGT" = "Linux") then
+cp -a $RESPATH/COPYING       $TMPPATH/AppDir/
+cp -a $RESPATH/README.txt    $TMPPATH/AppDir/  # if needed
+#cp -a $RESPATH/Changelog.txt $TMPPATH/AppDir/  # if needed
+fi
 
-if (test "$PKG" = "Linux" && (test "$CPU" = "x86_64" || test "$CPU" = "i686")) then # skip on ARM&RISC-V
-   echo "makePkg.sh: generating the AppImage for PowerBudget (about 20\") ..."
+echo "Copying binary ..."
+if [[ "$TGT" = "Linux" && "$BIT" = "64" ]]; then
+   if (test "$BINCLI" != "") then
+      cp -a $BINPATH/$BINCLI $TMPPATH/$APP/
+      cp -a $BINPATH/$BINCLI $TMPPATH/AppDir/$TMPBINP/
+   fi
+   if (test "$BINGUI" != "") then
+      cp -a $BINPATH/$BINGUI $TMPPATH/$APP/
+      cp -a $BINPATH/$BINGUI $TMPPATH/AppDir/$TMPBINP/
+   fi
+fi
+if [[ "$TGT" = "Linux" && "$BIT" = "32" ]]; then
+   if (test "$BINCLI" != "") then
+      cp -a $BINPATH/$BINCLI $TMPPATH/$APP/
+      cp -a $BINPATH/$BINCLI $TMPPATH/AppDir/$TMPBINP/
+   fi
+   if (test "$BINGUI" != "") then
+      cp -a $BINPATH/$BINGUI $TMPPATH/$APP/
+      cp -a $BINPATH/$BINGUI $TMPPATH/AppDir/$TMPBINP/
+   fi
+fi
+
+if [[ "$TGT" = "WinMgw" && "$BIT" = "64" ]]; then
+   if (test "$BINCLI" != "") then
+      cp -a $BINPATH/$BINCLI$EXT $TMPPATH/$APP/
+   fi
+   if (test "$BINGUI" != "") then
+      cp -a $BINPATH/$BINGUI$EXT $TMPPATH/$APP/
+   fi
+   echo "Copying deps ..."
+   for DLL in $DEPSLISTMGW ; do
+      cp -a $DEPPATH/$DLL $TMPPATH/$APP/
+   done
+fi
+if [[ "$TGT" = "WinMgw" && "$BIT" = "32" ]]; then
+   if (test "$BINCLI" != "") then
+      cp -a $BINPATH/$BINCLI$EXT $TMPPATH/$APP/
+   fi
+   if (test "$BINGUI" != "") then
+      cp -a $BINPATH/$BINGUI$EXT $TMPPATH/$APP/
+   fi
+   echo "Copying deps ..."
+   for DLL in $DEPSLISTMGW ; do
+      if [[ "$DLL" = "libgcc_s_seh-1.dll" ]]; then continue ; fi
+      cp -a $DEPPATH/$DLL $TMPPATH/$APP/
+   done
+   cp -a $DEPPATH/libgcc_s_dw2-1.dll $TMPPATH/$APP/
+fi
+
+if [[ "$TGT" = "WinMxe" && "$BIT" = "64" ]]; then
+   if (test "$BINCLI" != "") then
+      cp -a $BINPATH/$BINCLI$EXT $TMPPATH/$APP/
+   fi
+   if (test "$BINGUI" != "") then
+      cp -a $BINPATH/$BINGUI$EXT $TMPPATH/$APP/
+   fi
+fi
+if [[ "$TGT" = "WinMxe" && "$BIT" = "32" ]]; then
+   if (test "$BINCLI" != "") then
+      cp -a $BINPATH/$BINCLI$EXT $TMPPATH/$APP/
+   fi
+   if (test "$BINGUI" != "") then
+      cp -a $BINPATH/$BINGUI$EXT $TMPPATH/$APP/
+   fi
+fi
+
+# remove unwanted files if any
+
+echo "Compressing package ..."
+cwd=`pwd`
+cd $TMPPATH
+if (test "$TGT" = "Linux") then
+   file="$PKGNAME.tgz"
+   echo "Creating package file:'$file' ..."
+   tar -cvaf $file $APP > /dev/null
+   echo "Package file:'$file' done"
+fi
+if (test "$TGT" = "WinMxe" || test "$TGT" = "WinMgw") then
+   file="$PKGNAME.7z"
+   echo "Creating package file:'$file' ..."
+   7z a -m0=lzma -mx=9 -r $file $APP > /dev/null
+   echo "Package file:'$file' done"
+fi
+#rm -r AppDir
+#rm -r PowerBudget
+mv $file $DSTPATH/
+echo "Release file: '$DSTPATH/$file'"
+
+# make AppImage
+if [[ ("$TGT" = "Linux") && ("$CPU" = "x86_64" || "$CPU" = "i686")]]; then # skip on ARM & RISC-V
+   echo "Generating the AppImage (about 20\") ..."
    if (test -f logWget$DATE.txt) then { rm logWget$DATE.txt ; } fi
    if (test "$BIT" = "64") then
       if (! test -x linuxdeploy-x86_64.AppImage) then
@@ -251,39 +409,19 @@ if (test "$PKG" = "Linux" && (test "$CPU" = "x86_64" || test "$CPU" = "i686")) t
       #   chmod +x linuxdeploy-plugin-gtk.sh
       #fi
       #pwd
-      ./linuxdeploy-x86_64.AppImage -e ../powerBudgetGuiLinux64 --appdir AppDir -i PowerBudget.png -d powerBudget.desktop --output appimage > logLinuxdeploy$DATE.txt
+      ./linuxdeploy-x86_64.AppImage -e $TMPPATH/AppDir/$TMPBINP/$BINGUI --appdir AppDir -i $TMPPATH/AppDir/$APP.png -d $TMPPATH/AppDir/$APP.desktop --output appimage > logLinuxdeploy$DATE.txt
       ret=$?
-      file=powerBudget${VER}_${DATE}_Linux_${CPU}_${BIT}bit.AppImage
+      #file=${APP}_${VER}_${DATE}_${TGT}_${CPU}_${BIT}bit.AppImage
+      file="$PKGNAME.AppImage"
       if (test "$ret" = "0") then
-         mv PowerBudgetGui-x86_64.AppImage ../$file
-         cp -a ../$file ../..
-         echo "AppImage created: $file"
+         echo "AppImage created: $DSTPATH/$file"
+         mv ${APP}Gui-x86_64.AppImage $DSTPATH/$file
       else
-         echo "AppImage failed: $file"
+         echo "AppImage failed: $DSTPATH/$file"
       fi
    fi
    if (test "$BIT" = "32") then
       echo "As now skip AppImage at 32 bit"
    fi
 fi
-if (test "$PKG" = "Linux") then
-   file=powerBudget${VER}_${DATE}_Linux_${CPU}_${BIT}bit.tgz
-   echo "Creating package file:'$file' ..."
-   if (test -f $file) then { rm $file ; } fi
-   tar -cvaf $file PowerBudget
-   mv $file ../..
-   echo "Package file:'$file' done"
-fi
-if (test "$PKG" = "WinMxe" || test "$PKG" = "WinMgw") then
-   file=powerBudget${VER}_${DATE}_${PKG}_${BIT}bit.7z
-   echo "Creating package file:'$file' ..."
-   if (test -f $file) then { rm $file ; } fi
-   7z a -m0=lzma -mx=9 -r $file PowerBudget > /dev/null
-   mv $file ../..
-   echo "Package file:'$file' done"
-fi
-rm -r AppDir
-ret=$?
-if (test "$ret" = "0") then
-   rm -r PowerBudget
-fi
+echo Done
